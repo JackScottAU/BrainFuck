7aa28dd2df2e332c5d026295e50e5cdffd54d947
Jack Scott <jack@pushdone.com>
2013-06-15 14:37:38 +1000

    Add the start of an interpreter. Does nothing yet.

---
+++ JackScottAU.BrainFuck.Library.Tests/Interpreter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace JackScottAU.BrainFuck.Library.Tests
+{
+	[TestClass]
+	public class Interpreter
+	{
+		[TestMethod]
+		public void SimpleNotLoopedProgram()
+		{
+			Assert.Inconclusive("TODO");
+		}
+	}
+}
---
+++ JackScottAU.BrainFuck.Library/Interpreter.cs
@@ -0,0 +1,50 @@
+﻿using JackScottAU.BrainFuck.Library.Types;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace JackScottAU.BrainFuck.Library.Interpreter
+{
+	class Interpreter
+	{
+		public Interpreter(List<IInstruction> program)
+		{
+			/// <summary>
+			/// Holds the working memory for the program.
+			/// </summary>
+			private Byte[] _memory;
+
+			/// <summary>
+			/// Where to get our input.
+			/// </summary>
+			private TextReader _inputStream;
+
+			/// <summary>
+			/// Where to put our output.
+			/// </summary>
+			private TextWriter _outputStream;
+
+			/// <summary>
+			/// Creates a new context for interpreting programs within.
+			/// </summary>
+			/// <param name="memorySize">How large our memory should be.</param>
+			/// <param name="inputStream">Where to get input.</param>
+			/// <param name="outputStream">Where to put output.</param>
+			public Interpreter(int memorySize, TextReader inputStream, TextWriter outputStream)
+			{
+				throw new NotImplementedException();
+			}
+
+			/// <summary>
+			/// Interprets a piece of code.
+			/// </summary>
+			/// <param name="syntaxTree">The AST representation of the code.</param>
+			public void Interpret(List<IInstruction> syntaxTree)
+			{
+				throw new NotImplementedException();
+			}
+		}
+	}
+}
