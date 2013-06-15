36dbc0d5df70d870f87a8be89192be15f4a62e60
Jack Scott <jack@pushdone.com>
2013-06-15 14:12:21 +1000

    Add a unit test project.

---
+++ JackScottAU.BrainFuck.Library.Tests/Parser.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace JackScottAU.BrainFuck.Library.Tests
+{
+	[TestClass]
+	public class Parser
+	{
+		[TestMethod]
+		public void SimpleNonLoopedProgram()
+		{
+			Assert.Inconclusive("TODO");
+		}
+	}
+}
