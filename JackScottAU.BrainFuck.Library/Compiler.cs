5ec0319f36c48a6cb3cf52a8b3c82139f16a6816
Jack Scott <jack@pushdone.com>
2013-06-15 16:43:49 +1000

    Change portable class library to non-portable so I can get access to System.Reflection.Emit. Add the start of a CIL compiler.

---
+++ JackScottAU.BrainFuck.Library/Compiler.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+using System.Reflection.Emit;
+
+namespace JackScottAU.BrainFuck.Library
+{
+	public class Compiler
+	{
+		public Compiler()
+		{
+
+		}
+
+		public void Compile()
+		{
+			AssemblyBuilder asm = AppDomain.CurrentDomain.DefineDynamicAssembly(
+				new AssemblyName("TestOutput"),
+				AssemblyBuilderAccess.RunAndSave);
+
+			ModuleBuilder mod = asm.DefineDynamicModule("TestOutput.exe",
+				"TestOutput.exe");
+			TypeBuilder type = mod.DefineType("Program", TypeAttributes.Class);
+
+			MethodBuilder main = type.DefineMethod("Main", MethodAttributes.Public | MethodAttributes.Static);
+			ILGenerator il = main.GetILGenerator();
+			il.Emit(OpCodes.Ldstr, "Hello world!");
+			il.Emit(OpCodes.Call, typeof(Console).GetMethod("WriteLine",
+				BindingFlags.Public | BindingFlags.Static,
+				null, new Type[] { typeof(String) }, null));
+			il.Emit(OpCodes.Ret);
+
+			type.CreateType();
+			asm.SetEntryPoint(main);
+			asm.Save("TestOutput.exe");
+		}
+	}
+}
--- JackScottAU.BrainFuck.Library/Properties/AssemblyInfo.cs
+++ JackScottAU.BrainFuck.Library/Properties/AssemblyInfo.cs
@@ -1,20 +1,26 @@
-﻿using System.Resources;
-using System.Reflection;
+﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("JackScottAU.BrainFuck.Library")]
+[assembly: AssemblyTitle("ClassLibrary1")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("JackScottAU.BrainFuck.Library")]
+[assembly: AssemblyProduct("ClassLibrary1")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: NeutralResourcesLanguage("en")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("5fd9f77a-8c61-4592-8328-17fb4f6a5907")]
 
 // Version information for an assembly consists of the following four values:
 //
