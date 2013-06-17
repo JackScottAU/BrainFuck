3d547b6c4bbfab11550e3df09d2d98da65ccb3d6
Jack Scott <jack@pushdone.com>
2013-06-17 17:22:52 +1000

    Not sure what happened there.

--- JackScottAU.BrainFuck.Library/Compiler.cs
+++ JackScottAU.BrainFuck.Library/Compiler.cs
@@ -4,27 +4,21 @@ using System.Linq;
 using System.Text;
 using System.Reflection;
 using System.Reflection.Emit;
+using JackScottAU.BrainFuck.Library.Types;
 
 namespace JackScottAU.BrainFuck.Library
 {
 	public class Compiler
 	{
-		public Compiler()
+		public static void Compile(List<IInstruction> syntaxTree, string outputFileName, string outputName)
 		{
+			AssemblyBuilder asm = AppDomain.CurrentDomain.DefineDynamicAssembly(new AssemblyName(outputName),AssemblyBuilderAccess.RunAndSave);
 
-		}
-
-		public void Compile()
-		{
-			AssemblyBuilder asm = AppDomain.CurrentDomain.DefineDynamicAssembly(
-				new AssemblyName("TestOutput"),
-				AssemblyBuilderAccess.RunAndSave);
-
-			ModuleBuilder mod = asm.DefineDynamicModule("TestOutput.exe",
-				"TestOutput.exe");
+			ModuleBuilder mod = asm.DefineDynamicModule(outputName, outputFileName);
 			TypeBuilder type = mod.DefineType("Program", TypeAttributes.Class);
 
 			MethodBuilder main = type.DefineMethod("Main", MethodAttributes.Public | MethodAttributes.Static);
+			
 			ILGenerator il = main.GetILGenerator();
 			il.Emit(OpCodes.Ldstr, "Hello world!");
 			il.Emit(OpCodes.Call, typeof(Console).GetMethod("WriteLine",
@@ -32,9 +26,10 @@ namespace JackScottAU.BrainFuck.Library
 				null, new Type[] { typeof(String) }, null));
 			il.Emit(OpCodes.Ret);
 
+			
 			type.CreateType();
 			asm.SetEntryPoint(main);
-			asm.Save("TestOutput.exe");
+			asm.Save(outputFileName);
 		}
 	}
 }
